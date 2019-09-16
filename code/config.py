2a215476f6753ce90945a486df970f114e0afca0
Ahmed-CH <55385335+Ahmed-CH@users.noreply.github.com>
2019-09-16 14:11:09 +0100

    Update config.py

--- code/config.py
+++ code/config.py
@@ -4,7 +4,7 @@
 FACEBOOK_TOKEN='EAANSoji4ySkBAIwkbqTZCIJE4neAzu9a1bLGtx5RD33nh7d9XZBHFZCXIAmdcvPQqvnmcHhampypQKZAUit42nWnLFjODNq1D05ZBCTZAjbmGvoA28qVmgcPamliTsqmjlkKBWcpMMAVaE3OyAkr74RYzll1uJ1gSJctKuZCZBhOPrXMZBusrZCvC3ZBGQsvOLcXSy76P4Q0aOgSQZDZD'
 
 ################
-####instagram we shoud use a reviewed APP###
+####instagram Token we shoud use a reviewed APP###
 ################
 INSTA_TOKEN="331728938.02f12f5.d1475f8b2bcd49aea93534295f70069f"
 INSTA_CLIENT_SECRET="a2872606505c483f8dd215c46e78200f"
