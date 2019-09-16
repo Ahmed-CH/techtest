f6dc55275d967f8cb7b1dac14d7fe5ef1946d2ea
Ahmed-CH <55385335+Ahmed-CH@users.noreply.github.com>
2019-09-16 15:14:23 +0100

    Update config.py

--- code/config.py
+++ code/config.py
@@ -1,12 +1,12 @@
 ###############
 ####Facebook Token ==> we shoud use a reviewed APP with "Page Public Content Access"###
 ###############
-FACEBOOK_TOKEN='EAANSoji4ySkBAIwkbqTZCIJE4neAzu9a1bLGtx5RD33nh7d9XZBHFZCXIAmdcvPQqvnmcHhampypQKZAUit42nWnLFjODNq1D05ZBCTZAjbmGvoA28qVmgcPamliTsqmjlkKBWcpMMAVaE3OyAkr74RYzll1uJ1gSJctKuZCZBhOPrXMZBusrZCvC3ZBGQsvOLcXSy76P4Q0aOgSQZDZD'
+FACEBOOK_TOKEN='FB_Token'
 
 ################
 ####instagram Token we shoud use a reviewed APP###
 ################
-INSTA_TOKEN="331728938.02f12f5.d1475f8b2bcd49aea93534295f70069f"
+INSTA_TOKEN="Insta_token"
 INSTA_CLIENT_SECRET="a2872606505c483f8dd215c46e78200f"
 
 ################
